a0e49655d06ba0cf25dc4cbee6a7762b4776aaa1
CaldeCrack <an.caldeguardia@gmail.com>
2026-07-23 02:34:33 -0400

    feat: dynamic zen browser theming
    Finally done, the documentation for zen-wabi and fx-autoconfig were so
    bad it took me a lot of time to finally get right

--- .config/matugen/post-hook-scripts/zen-wabi.sh
+++ .config/matugen/post-hook-scripts/zen-wabi.sh
@@ -1,3 +1,3 @@
 PROFILE_DIR="$(find "$HOME/.config/zen" -maxdepth 1 -type d -name "*.Default (release)" 2>/dev/null | head -n 1)"
 
-cp ~/.cache/matugen/userChrome.css "$PROFILE_DIR/chrome/userChrome.css"
+cp ~/.cache/matugen/matugen-vars.json "$PROFILE_DIR/chrome/matugen-vars.json"
--- .config/matugen/pre-hook-scripts/zen-browser.sh
+++ .config/matugen/pre-hook-scripts/zen-browser.sh
@@ -1,3 +1,3 @@
-PROFILE_DIR="$(find "$HOME/.zen" -maxdepth 1 -type d -name "*.Default (release)" 2>/dev/null | head -n 1)"
+PROFILE_DIR="$(find "$HOME/.config/zen" -maxdepth 1 -type d -name "*.Default (release)" 2>/dev/null | head -n 1)"
 
 mkdir -p "$PROFILE_DIR/chrome"
