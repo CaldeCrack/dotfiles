50a12d5fd92cab56fc8977c5759c15a4cc86e174
CaldeCrack <an.caldeguardia@gmail.com>
2026-07-23 20:37:43 -0400

    fix: terminal background opacity when running TUIs
    When running a command simply like `kitty nvim` it loses the background
    opacity that should have, fix this via sourcing the terminal colors
    before running the program

--- .config/hypr/config/programs.lua
+++ .config/hypr/config/programs.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.terminal = "kitty"
-M.fileManager = "thunar"
+M.fileManager = "kitty ~/.config/hypr/scripts/kitty-run.sh spf"
 
 M.menuName = "rofi"
 M.menu = M.menuName .. " -show drun -modi drun -show-icons"
@@ -13,7 +13,7 @@ M.browser = "zen-browser"
 M.discord = "vesktop"
 M.office = "onlyoffice-desktopeditors"
 M.volumeMixer = "pavucontrol"
-M.wifi = "kitty nmtui"
+M.wifi = "kitty ~/.config/hypr/scripts/kitty-run.sh nmtui"
 
 M.bar = "ags run ~/.config/ags/app.tsx"
 
---
+++ .config/hypr/scripts/kitty-run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+source "$HOME/.cache/matugen/variables.sh"
+sh "$HOME/.cache/matugen/terminal.sh"
+exec "$@"
