2c6ae2d9c7dcb23a196889bcf0284c3a7194b8c6
CaldeCrack <an.caldeguardia@gmail.com>
2026-09-02 00:35:41 -0400

    fix: remove hardcoded home folders and config options

--- .config/hypr/config/env.lua
+++ .config/hypr/config/env.lua
@@ -1,5 +1,5 @@
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("HYPRSHOT_DIR", "/home/caldecrack/Pictures/Screenshots")
+hl.env("HYPRSHOT_DIR", os.getenv("HOME") .. "/Pictures/Screenshots")
 hl.env("QT_QPA_PLATFORM", "wayland")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
